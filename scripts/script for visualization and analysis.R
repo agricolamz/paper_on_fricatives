edaf592a62f3859c2000e67c4aeb03123c06526c
agricolamz <agricolamz@gmail.com>
2017-11-16 02:47:40 +0300

    add slope and maximum

--- scripts/script for visualization and analysis.R
+++ scripts/script for visualization and analysis.R
@@ -8,50 +8,123 @@ write_lines(LPC_df, "LPC_results.csv")
 rm(LPC_df)
 
 LPC_df <- read_tsv("LPC_results.csv", col_names = FALSE)
-names(LPC_df) <- c("frequency", "power", "file_name", "token")
+names(LPC_df) <- c("Hertz", "power", "soundname", "token")
+
+selected <- read_tsv("selection.tsv")
+
+LPC_df %>% 
+  semi_join(selected) ->
+  LPC_df
+
+write_lines(LPC_df, "LPC_df_selected.tsv")
+
+LPC_df <- read_tsv("LPC_df_selected.tsv")
 
 LPC_df$utterance <- str_extract(LPC_df$token, "1|2|3|4|5|6|cf")
-LPC_df$language <- str_extract(LPC_df$file_name, "kabardian|adyghe|nanai|udmurt|chukchi|russian")
-LPC_df$dictor <- str_extract(LPC_df$file_name, "_d.{1,2}_")
+LPC_df$language <- str_extract(LPC_df$soundname, "kabardian|adyghe|nanai|udmurt|chukchi|russian|ubykh")
+LPC_df$dictor <- str_extract(LPC_df$soundname, "_d.{1,2}")
+LPC_df$dictor <- str_replace_all(LPC_df$dictor, "_", "")
+LPC_df$Bark <- 13*atan(0.00076*LPC_df$Hertz) +
+  3.5*atan((LPC_df$Hertz/7500)^2)
+names(LPC_df)[1] <- "Hertz"
 
-cepstral_df <- read_lines("cepstral_results.csv")
-cepstral_df <- cepstral_df[!str_detect(cepstral_df, "freq\\(Hz\\)")]
-write_lines(cepstral_df, "cepstral_results.csv")
-rm(cepstral_df)
-
-cepstral_df <- read_tsv("cepstral_results.csv", col_names = FALSE)
-names(cepstral_df) <- c("frequency", "power", "file_name", "token")
-
-cepstral_df$utterance <- str_extract(cepstral_df$token, "1|2|3|4|5|6|cf")
-cepstral_df$language <- str_extract(cepstral_df$file_name, "kabardian|adyghe|nanai|udmurt|chukchi|russian")
-cepstral_df$dictor <- str_extract(cepstral_df$file_name, "_d.{1,2}_")
+# cepstral_df <- read_lines("cepstral_results.csv")
+# cepstral_df <- cepstral_df[!str_detect(cepstral_df, "freq\\(Hz\\)")]
+# write_lines(cepstral_df, "cepstral_results.csv")
+# rm(cepstral_df)
+# 
+# cepstral_df <- read_tsv("cepstral_results.csv", col_names = FALSE)
+# names(cepstral_df) <- c("Hertz", "power", "file_name", "token")
+# 
+# cepstral_df$utterance <- str_extract(cepstral_df$token, "1|2|3|4|5|6|cf")
+# cepstral_df$language <- str_extract(cepstral_df$soundname, "kabardian|adyghe|nanai|udmurt|chukchi|russian|ubykh")
+# cepstral_df$dictor <- str_extract(cepstral_df$soundname, "_d.{1,2}_")
 
 # draw the plot -----------------------------------------------------------
 LPC_df %>% 
-  #slice(1:136732*58) %>% 
   filter(language == "udmurt") %>%
-  ggplot(aes(x = frequency, 
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
              y = power,
              color = token))+
   geom_line()+
   theme_bw()+
   theme(legend.position = "none")+
-  labs(title = "LPC smoothing for different speakers")+
-  facet_wrap(~dictor)+
-  ylim(-20, 60)
+  labs(title = "LPC smoothing for different Udmurt speakers")+
+  facet_grid(dictor~measurement, scales = "free")
 
-cepstral_df %>% 
-  ggplot(aes(x = frequency, 
+LPC_df %>% 
+  filter(language == "adyghe") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
              y = power,
              color = token))+
   geom_line()+
   theme_bw()+
-  labs(title = "Cepstral smoothing for different speakers")+
-  facet_wrap(~dictor)
+  theme(legend.position = "none")+
+  labs(title = "LPC smoothing for different Adyghe speakers")+
+  facet_grid(dictor~measurement, scales = "free")
+
+LPC_df %>% 
+  filter(language == "chukchi") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
+             y = power,
+             color = token))+
+  geom_line()+
+  theme_bw()+
+  theme(legend.position = "none")+
+  labs(title = "LPC smoothing for different Chukchi speakers")+
+  facet_grid(dictor~measurement, scales = "free")
+
+LPC_df %>% 
+  filter(language == "nanai") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
+             y = power,
+             color = token))+
+  geom_line()+
+  theme_bw()+
+  theme(legend.position = "none")+
+  labs(title = "LPC smoothing for different Nanai speakers")+
+  facet_grid(dictor~measurement, scales = "free")
+
+LPC_df %>% 
+  filter(language == "russian") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
+             y = power,
+             color = token))+
+  geom_line()+
+  theme_bw()+
+  theme(legend.position = "none")+
+  labs(title = "LPC smoothing for different Russian speakers")+
+  facet_grid(dictor~measurement, scales = "free")
+
+LPC_df %>% 
+  filter(language == "ubykh") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
+             y = power,
+             color = token))+
+  geom_line()+
+  theme_bw()+
+  theme(legend.position = "none")+
+  labs(title = "LPC smoothing for different Ubykh speakers")+
+  facet_grid(dictor~measurement, scales = "free")
+
+# cepstral_df %>% 
+#   ggplot(aes(x = Hertz, 
+#              y = power,
+#              color = token))+
+#   geom_line()+
+#   theme_bw()+
+#   labs(title = "Cepstral smoothing for different speakers")+
+#   facet_wrap(~dictor)
 
 LPC_df %>%
   filter(grepl("chukchi", language)) %>%
-  ggplot(aes(x = frequency, 
+  ggplot(aes(x = Hertz, 
              y = power))+
   stat_summary(fun.data ="mean_sdl", geom = "smooth")+
   theme_bw()+
@@ -59,21 +132,21 @@ LPC_df %>%
   labs(title = "LPC smoothing for speakers of Chukchi")+
   facet_wrap(~language)
 
-LPC_df %>%
-    ggplot(aes(x = `frequency (Hz)`, 
-               y = `power (Db/Hz)`))+
-    stat_summary(fun.data ="mean_sdl", geom = "smooth")+
-    theme_bw()+
-    labs(title = "LPC smoothing for different speakers")
+# LPC_df %>%
+#     ggplot(aes(x = `Hertz (Hz)`, 
+#                y = `power (Db/Hz)`))+
+#     stat_summary(fun.data ="mean_sdl", geom = "smooth")+
+#     theme_bw()+
+#     labs(title = "LPC smoothing for different speakers")
 
 LPC_df %>%
-  ggplot(aes(x = frequency, 
+  ggplot(aes(x = Hertz, 
              y = power))+
   geom_line()+
   geom_smooth(method = "lm",formula = y ~ poly(x, 7))
   
 
-fit <- lm(frequency~poly(power, 7), data = LPC_df)
+fit <- lm(Hertz~poly(power, 7), data = LPC_df)
 summary(fit)
 
 
@@ -99,11 +172,25 @@ df %>%
   filter(dictor != "d32")->
   df
 
+df$cog_bark <- 13*atan(0.00076*df$cog) +
+  3.5*atan((df$cog/7500)^2)
+
 df %>% 
-  ggplot(aes(dictor, cog, color = language))+
+  ggplot(aes(dictor, cog))+
+  geom_boxplot(aes(fill = language))+
   geom_point()+
   theme_bw()+
-  ggtitle("Center of Gravity") -> picture_1
+  ggtitle("Center of Gravity (in Hertz)")
+
+df %>% 
+  ggplot(aes(dictor, cog_bark))+
+  geom_boxplot(aes(fill = language))+
+  geom_point()+
+  theme_bw()+
+  ggtitle("Center of Gravity (in Bark)")
+
+
+
 
 df %>% 
   filter(position == "asa" |
@@ -117,15 +204,29 @@ df %>%
   ggtitle("Center of Gravity") -> picture_2
 
 df %>% 
-  filter(position != "asa" |
-           position != "əsa" |
-           position != "isa" |
-           position != "#sa" |
-           position != "#sɐ") %>%
-  ggplot(aes(dictor, cog, color = language))+
+  filter(position == "asa" |
+           position == "əsa" |
+           position == "isa" |
+           position == "#sa" |
+           position == "#sɐ") %>%
+  ggplot(aes(dictor, cog))+
+  geom_boxplot(aes(fill = language))+
   geom_point()+
   theme_bw()+
-  ggtitle("Center of Gravity") -> picture_3
+  ggtitle("Center of Gravity (before [a]) in Hertz")
+
+df %>% 
+  filter(position == "asa" |
+           position == "əsa" |
+           position == "isa" |
+           position == "#sa" |
+           position == "#sɐ") %>%
+  ggplot(aes(dictor, cog_bark))+
+  geom_boxplot(aes(fill = language))+
+  geom_point()+
+  theme_bw()+
+  ggtitle("Center of Gravity (before [a]) in Bark")
+
 
 df %>% 
   ggplot(aes(dictor, sd, color = language))+
@@ -145,7 +246,6 @@ df %>%
   theme_bw()+
   ggtitle("Kurtosis")
 
-write_tsv(df, "selection.tsv")
 selected <- read_tsv("selection.tsv")
 
 
@@ -167,7 +267,49 @@ selected %>%
        subtitle = "sample from all our data")
 
 selected %>% 
-  group_by(dictor) %>% 
-  summarise(iqr = IQR(cog_bark)*1.5,
-            median = median(cog_bark)) %>% 
-  right_join(selected) ->
+  ggplot(aes(dictor, cog))+
+  geom_boxplot(aes(fill = language))+
+  geom_point()+
+  theme_bw()+
+  labs(title = "Center of Gravity (in Hertz)",
+       subtitle = "sample from all our data")
+
+
+# get slopes and max ------------------------------------------------------
+
+LPC_df %>% 
+  filter(dictor == "d9") %>%
+  gather(key = measurement, value = value, c(Hertz, Bark)) %>% 
+  ggplot(aes(x = value, 
+             y = power,
+             color = token))+
+  geom_line()+
+  theme_bw()+
+  labs(title = "LPC smoothing for different Udmurt speakers")+
+  facet_grid(~measurement, scales = "free")
+
+LPC_df %>% 
+  filter(dictor == "d9",
+         Bark > 10,
+         Bark < 20.5) %>%
+  group_by(token) %>% 
+  summarise(min = which(power == min(power)),
+            max = which(power == max(power)),
+            min_Bark = Bark[which(power == min(power))],
+            max_Bark = Bark[which(power == max(power))],
+            Bark_slope = lm(power[min:max]~Bark[min:max])$coefficients[2]) %>% 
+  select(token, max_Bark, Bark_slope) %>% 
+  View()
+
+LPC_df %>% 
+  filter(dictor == "d9",
+         Hertz > 1000,
+         Hertz < 7000) %>%
+  group_by(token) %>% 
+  summarise(min = which(power == min(power)),
+            max = which(power == max(power)),
+            min_Hertz = Hertz[which(power == min(power))],
+            max_Hertz = Hertz[which(power == max(power))],
+            Hertz_slope = lm(power[min:max]~Hertz[min:max])$coefficients[2]) %>% 
+  select(max_Hertz, Hertz_slope) %>% 
+  View()
